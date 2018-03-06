9836f7d73d14682295806791538754f879fa913a
Jim <jim@advancebass.com>
2018-03-05 19:02:49 -0800

    init

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About Us</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate lectus sed elit feugiat fringilla non facilisis erat. Morbi congue turpis sit amet metus elementum pretium. Nulla elementum urna id mauris rutrum accumsan. Donec sem metus, porttitor ac facilisis sed, aliquet eu arcu. Donec id felis ut erat ultrices tincidunt. Aliquam erat volutpat. Fusce eu dolor odio. Praesent a fringilla orci. Nunc ac luctus sapien. Suspendisse semper bibendum mauris id ullamcorper. Pellentesque vitae felis a urna hendrerit ultricies id et elit. In hac habitasse platea dictumst. Cras massa nulla, egestas facilisis laoreet vel, rhoncus nec leo.
+                </p>
+            </div>
+        );
+    }
+}
+
+About.propTypes = {};
+
+export default About;
